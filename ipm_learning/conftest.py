dbf83bddb558545a061b3a1f6ac9fe31c91f2132
Mark Swaringen <mswaringen@gmail.com>
2021-12-08 13:22:56 +0100

    init

---
+++ ipm_learning/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from ipm_learning.users.models import User
+from ipm_learning.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
