0d45c050cf297d6c96efd0df23d82482b0146ba1
mark swaringen <mswaringen@gmail.com>
2022-02-03 14:54:11 +0100

    about plus

--- ipm_learning/content/urls.py
+++ ipm_learning/content/urls.py
@@ -5,7 +5,7 @@ app_name = "content"
 
 urlpatterns = [
     path("courses/", CourseListView.as_view(), name="course-list"),
-    path("events/", EventListView.as_view(), name="event-list"),
+    # path("events/", EventListView.as_view(), name="event-list"),
     path("library/", CourseLibraryView.as_view(), name="course-library"),
     path("<slug>/", CourseDetailView.as_view(), name="course-detail"),
     path("<slug>/<content_slug>/", ContentDetailView.as_view(), name="content-detail"),
--- ipm_learning/pages/admin.py
+++ ipm_learning/pages/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import TeamMember, Testimonial
+from .models import PageContent, TeamMember, Testimonial
 
 # Register your models here.
 
 admin.site.register(Testimonial)
 admin.site.register(TeamMember)
+admin.site.register(PageContent)
