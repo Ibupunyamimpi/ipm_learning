13a64863d890a74afb93881b8be4172121ca5473
mark swaringen <mswaringen@gmail.com>
2022-03-17 13:38:09 +0100

    user upload and pwd reset email

--- ipm_learning/users/admin.py
+++ ipm_learning/users/admin.py
@@ -7,16 +7,78 @@ from import_export.admin import ImportExportModelAdmin
 
 from ipm_learning.users.forms import UserChangeForm, UserCreationForm
 
+from django.http import HttpRequest
+from allauth.account.views import PasswordResetView
+from django.middleware.csrf import get_token
+from django.conf import settings
+
 User = get_user_model()
 
 class UserUploadResource(resources.ModelResource):
     class Meta:
         model = User
+        
+    # def after_save_instance(self, instance, using_transactions, dry_run):
+    #     print(self, instance)
+        
+    #     def send_reset_password_email(instance):
+            
+    #         def has_group(user, group):
+    #             return user.groups.filter(id=group).exists()
+        
+    #         u = instance
+    #         # group = "Upload-Users"
+    #         group = 2
+            
+    #         print(u, u.groups.all())
+                
+    #         if has_group(u, group): # Or has_groups(self.request.user, ["HelloDjango", "TEST"])
+    #             print(u,group)
+    #             # First create a post request to pass to the view
+    #             request = HttpRequest()
+    #             request.method = 'POST'
 
+    #             # add the absolute url to be be included in email
+    #             if settings.DEBUG:
+    #                 request.META['HTTP_HOST'] = '127.0.0.1:8000'
+    #             else:
+    #                 request.META['HTTP_HOST'] = 'ibupunyamimpi.org'
+
+    #             # pass the post form data
+    #             request.POST = {
+    #                 'email': instance.email,
+    #                 'csrfmiddlewaretoken': get_token(HttpRequest())
+    #             }
+    #             PasswordResetView.as_view()(request)  # email will be sent!
+            
+    #     if dry_run is False:
+    #         send_reset_password_email(instance)
+            
+    
+            
+    
+                
+                
+    # def after_save_instance(
+    #     self, instance: User, using_transactions: bool, dry_run: bool,
+    # ):
+    #     super().after_save_instance(instance, using_transactions, dry_run)
+    #     print('after_save')
+    #     if dry_run is False:
+    #         send_reset_password_email(instance)
+                
+                
 
 @admin.register(User)
 class UserAdmin(ImportExportModelAdmin):
 
+    def group(self, user):
+        groups = []
+        for group in user.groups.all():
+            groups.append(group.name)
+        return ' '.join(groups)
+        groups.short_description = 'Groups'
+    
     form = UserChangeForm
     add_form = UserCreationForm
     fieldsets = (
@@ -29,7 +91,7 @@ class UserAdmin(ImportExportModelAdmin):
                     "is_active",
                     "is_staff",
                     "is_superuser",
-                    "groups",
+                    # "groups",
                     "user_permissions",
                 ),
             },
@@ -41,7 +103,12 @@ class UserAdmin(ImportExportModelAdmin):
         (None, {
             'fields': ('phone_number'),}),)
     
-    list_display = ["username", "name", "email", "is_superuser"]
+    list_display = ["username", "name", "email", "group", "is_superuser"]
+    list_filter = ["groups"]
     search_fields = ["name"]
+    
+    
 
+# @admin.register(User)
 # class UserAdmin(auth_admin.UserAdmin):
+#     pass
--- ipm_learning/users/models.py
+++ ipm_learning/users/models.py
@@ -4,6 +4,15 @@ from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 
+from allauth.account.views import PasswordResetView
+from django.conf import settings
+from django.dispatch import receiver
+from django.http import HttpRequest
+from django.middleware.csrf import get_token
+from django.db.models.signals import post_save
+from django.db import transaction
+
+
 class User(AbstractUser):
     """Default user for ipm_learning."""
 
@@ -21,3 +30,42 @@ class User(AbstractUser):
 
         """
         return reverse("users:detail", kwargs={"username": self.username})
+
+
+def on_transaction_commit(func):
+    def inner(*args, **kwargs):
+        transaction.on_commit(lambda: func(*args, **kwargs))
+    return inner
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+@on_transaction_commit
+def send_reset_password_email(sender, instance, created, **kwargs):
+    
+    def has_group(user, group):
+        return user.groups.filter(name=group).exists()
+    
+    u = instance
+    group = "Upload-Users"
+    # group = 2
+    
+    print(u, u.groups.all())
+        
+    if created:
+        if has_group(u, group): # Or has_groups(self.request.user, ["HelloDjango", "TEST"])
+            print(u,group)
+            # First create a post request to pass to the view
+            request = HttpRequest()
+            request.method = 'POST'
+
+            # add the absolute url to be be included in email
+            if settings.DEBUG:
+                request.META['HTTP_HOST'] = '127.0.0.1:8000'
+            else:
+                request.META['HTTP_HOST'] = 'ibupunyamimpi.org'
+
+            # pass the post form data
+            request.POST = {
+                'email': instance.email,
+                'csrfmiddlewaretoken': get_token(HttpRequest())
+            }
+            PasswordResetView.as_view()(request)  # email will be sent!
