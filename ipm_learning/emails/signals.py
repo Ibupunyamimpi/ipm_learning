89e8310119fd303d0cda35221c5508871ea8c238
mark swaringen <mswaringen@gmail.com>
2022-03-18 14:41:15 +0100

    bulk upload and email enabled

--- ipm_learning/emails/signals.py
+++ ipm_learning/emails/signals.py
@@ -17,7 +17,13 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-# Helper method ensures instance is committed before calling post_save
+"""
+
+Helper Methods
+
+"""
+
+# Ensures instance is committed before calling post_save
 def on_transaction_commit(func):
     def inner(*args, **kwargs):
         transaction.on_commit(lambda: func(*args, **kwargs))
@@ -26,11 +32,17 @@ def on_transaction_commit(func):
 def email_sender(email, user):
     html_message = render_to_string('emails/email_template.html', {'context': email.text_content})
     plain_message = strip_tags(html_message)
-    subject= email.subject
+    subject= "[Ibu Punya Mimpi] " + email.subject
     from_email= None
     to = user.email
     send_mail(subject, plain_message, from_email, [to], html_message=html_message)
 
+
+"""
+
+CourseEmail
+
+"""
 
 # Send CourseEmail immediately
 @receiver(post_save, sender=CourseEmail)
@@ -41,21 +53,11 @@ def send_course_email_now(sender, instance, created, **kwargs):
         course_records = course.course_records.all()
         for cr in course_records:
             email_sender(email, cr.user)
+        email.send_now = False
+        email.save()
  
-
-# Send GroupEmail immediately
-@receiver(post_save, sender=GroupEmail)
-def send_group_email_now(sender, instance, created, **kwargs):
-    email = instance
-    if email.send_now:
-        group = email.group
-        print(group)
-        users = User.objects.filter(groups__name=group)
-        for user in users:
-            email_sender(email, user)
-
-
-# Triggers Course Emails
+ 
+ # Triggers Course Emails on CourseRecord create
 @receiver(post_save,sender=CourseRecord)
 def send_course_email(sender, instance, created, **kwargs):    
     
@@ -67,7 +69,27 @@ def send_course_email(sender, instance, created, **kwargs):
             email_sender(email, user)
 
 
-# Triggers Group Emails
+
+"""
+
+GroupEmail
+
+"""
+
+# Send GroupEmail immediately
+@receiver(post_save, sender=GroupEmail)
+def send_group_email_now(sender, instance, created, **kwargs):
+    email = instance
+    if email.send_now:
+        group = email.group
+        print(group)
+        users = User.objects.filter(groups__name=group)
+        for user in users:
+            email_sender(email, user)
+        email.send_now = False
+        email.save()
+
+# Triggers Group Emails on User create
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 @on_transaction_commit
 def send_group_email(sender, instance, created, **kwargs):
@@ -82,7 +104,7 @@ def send_group_email(sender, instance, created, **kwargs):
                 email_sender(email, user)
 
 
-# Triggers password reset email
+# Triggers password reset email for Users created in the "Upload-Users" Group
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 @on_transaction_commit
 def send_reset_password_email(sender, instance, created, **kwargs):
